210603b20dc8a59dc14c5d9613e52263f155ff7b
Gabriel Donizetti <donizettigabriel3@gmail.com>
2025-04-07 21:22:15 -0300

    meu projeto conta banco

--- ContaBanco/ContaTerminal.java
+++ ContaBanco/ContaTerminal.java
@@ -1,8 +1,35 @@
 package ContaBanco;
 
+import java.util.Scanner;
+
 public class ContaTerminal {
-            public static void main (String[] args) throws Exception {
-                System.out.println("Olá, Mundo!");
-        }
+            public static void main (String[] args) {
+                Scanner scanner = new Scanner(System.in);
+
+                System.out.println("Por favor digite os dados da conta bancária");
+
+                System.out.print("Número da agência(Texto - Ex: 067-3):");
+                      String agencia = scanner.nextLine();
+                System.out.print("Número da conta(Texto - Ex: 1234-5):");
+                       int numeroContaBanco = scanner.nextInt();
+                System.out.print("Nome do Cliente(Texto - Ex: <NAME> ):");
+
+                String nomeCliente = scanner.nextLine();
+
+                System.out.print("Saldo da conta(Numero - Ex: 1000.00):");
+                    double saldo = scanner.nextDouble();
+
+                System.out.print("Numero da conta(Numero - Ex: 1234-5):");
+                double numeroConta = scanner.nextDouble();
+
+                System.out.println("\nDados da Conta");
+                System.out.println("Agência: " + agencia);
+                System.out.println("Numero da conta: " + numeroContaBanco);
+                System.out.println("Nome do Cliente: " + nomeCliente);
+                System.out.println("Saldo: " + saldo);
+                System.out.println("Numero da conta: " + numeroConta);
+
     }
- 
+
+
+}
